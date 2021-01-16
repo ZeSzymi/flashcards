610b1aabc3ed2315ea7a8484fe1afb0f329f83da
ZeSzymi <szymek.urbanik@gmail.com>
2021-01-16 15:08:53 +0100

    logger added to healthcheck

--- Flashcards/Flashcards/Controllers/HealthCheckController.cs
+++ Flashcards/Flashcards/Controllers/HealthCheckController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Net;
 
@@ -8,10 +9,18 @@ namespace flashcards.Controllers
     [Route("api/[controller]")]
     public class HealthCheckController : Controller
     {
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
         [AllowAnonymous]
         [HttpGet()] 
         public IActionResult Get() 
         {
+            _logger.LogTrace("healthcheck");
             return Ok("ok");
         }
 
@@ -19,6 +28,7 @@ namespace flashcards.Controllers
         [HttpGet("ip")]
         public IActionResult GetIp()
         {
+            _logger.LogTrace("ip");
             var externalip = new WebClient().DownloadString("http://icanhazip.com");
             return Ok(externalip);
         }
@@ -27,6 +37,7 @@ namespace flashcards.Controllers
         [HttpGet("folder")]
         public IActionResult GetFolder()
         {
+            _logger.LogTrace("folder");
             string path = Directory.GetCurrentDirectory();
             return Ok(path);
         }
